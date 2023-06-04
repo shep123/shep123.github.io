f06f441497991fdabbd6757e78a2d8a6a6e3757b
Shep <shep123@gmail.com>
2023-06-04 16:53:50 -0400

    Initial page creation

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// set variables for bomb-omb explosion image swap
+// To-do, figure out how to prevent explosion.webp from looping infinitely.
+var onImg= "images/bomb-omb.gif";
+var offImg= "images/explosion.webp";
